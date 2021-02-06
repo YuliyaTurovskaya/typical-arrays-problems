8f93b8e70cacd2c4a4d463977ab3cbabcaabced5
turowskaja@gmail.com <turowskaja@gmail.com>
2021-02-06 20:05:47 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array === undefined || array[0] === undefined ) ? 0 : array.sort((a,b) => a-b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array === undefined || array[0] === undefined ) ? 0 : array.sort((a,b) => a-b)[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array === undefined || array[0] === undefined ) ? 0 : (array.reduce((sum, current) => sum + current, 0))/array.length;
 }
